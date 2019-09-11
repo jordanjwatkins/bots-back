8d3275d463ae87bd555b135be80795d05b789c1a
jordanjwatkins <coolcatpad@hotmail.com>
2019-09-10 22:07:22 -0700

    Delete line 1

--- src/js/scenes/line-1/images.js
+++
@@ -1,9 +0,0 @@
-import loadImages from '../../libs/loadImages'
-
-import spritesheet from './src/images/spritesheet.png'
-
-const imagesToLoad = {
-  spritesheet,
-}
-
-export default loadImages(imagesToLoad)
